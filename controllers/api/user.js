3702f77218707ba2ae5ae7751c03dca432ad6017
Adam McLane <mclanea92@gmail.com>
2022-06-15 21:13:45 -0400

    fixing git

--- controllers/api/user.js
+++ controllers/api/user.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const res = require('express/lib/response');
 const User = require('../../models');
 
-router.post('/login', async (req, res) => {
+router.post('/login', async (req, res) => { 
     try {
       const userData = await User.findOne({ where: { email: req.body.email } });
   
