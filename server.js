128f156d4a28b7b445e2575e28bf7d251717e448
Steffan Carter <xisled09@gmail.com>
2022-06-17 19:34:54 -0400

    changes to package and controllers

--- server.js
+++ server.js
@@ -1,21 +1,31 @@
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connections');
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+// const exphbs = require('express-handlebars');
+// const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+// const sess = {
+//     secret: 'Super secret secret',
+//     resave: false,
+//     saveUninitialized: true,
+//   };
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(session(sess));
+
+// const hbs = exphbs.create({ })
+
+// app.engine('handlebars', hbs.engine);
+// app.set('view engine', 'handlebars');
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening on http://localhost:3001/'));
 });
-// lee message me if you're still awake <-----------------------------------------------------------
