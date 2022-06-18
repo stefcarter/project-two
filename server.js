291574359f988e27856481286a3ab6259f1c9654
Adam McLane <mclanea92@gmail.com>
2022-06-18 11:07:24 -0400

    updated merge

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const session = require('express-session')
 const SequelizeStore = require('connect-session-sequelize')(session.Store)
 const hbs = exphbs.create({ helpers });
 
-
+// for new push
 const app = express();
 const PORT = process.env.PORT || 3001;
 
