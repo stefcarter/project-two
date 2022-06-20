fc248fc7203f0ef5bb3dd0de2a0db4223e757228
Steffan Carter <xisled09@gmail.com>
2022-06-19 23:40:36 -0400

    Style error fixed

--- public/js/script.js
+++ public/js/script.js
@@ -12,4 +12,28 @@ function hoverOffSiderbar(){
     document.getElementById("sideBar").style.width ="4%";
 }
 
-
+// API for cover art
+function getApi() {
+    // encodeURIcomponent
+    fetch('https://www.giantbomb.com/api/search/?api_key=0f72464a2a563dabc8eda47f92f607713cfee4c3&format=json&query="' + newName.value + '"&resources=game')
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        repoList.innerHTML = ''
+        for (var i = 0; i < 5; i++) {
+          var listItem = document.createElement('li');
+          var listImg = document.createElement('img');
+        //   var listText = document.createElement('p');
+          listImg.setAttribute('src', results.data[i].images.original_url);
+          listImg.setAttribute('alt', "title");
+  
+        //   listItem.appendChild(listImg);
+        //   listText.textContent = data.data[i].title;
+        //   listItem.appendChild(listText);
+          repoList.appendChild(listItem);
+        }
+        // console.log(data)
+      });
+  }
+  fetchButton.addEventListener('click', getApi);
