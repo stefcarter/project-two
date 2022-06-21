58c69ec4066485d8eb9b4150411b39ffbcfdad9c
Adam McLane <mclanea92@gmail.com>
2022-06-21 13:37:32 -0400

    updated api to show coverart and title

--- public/js/script.js
+++ public/js/script.js
@@ -1,16 +1,3 @@
-//Hovering Sidebar code below
-// var miniIcon =true;
-
-// document.getElementbyId("sideBar").addEventlistner("mouseover",mouseOver);
-// document.getElementbyId("sideBar").addEventlistner("mouseout", mouseOut);
-
-// function hoverOnSidebar(){
-//     document.getElementById("sideBar").style.width ="15%";
-// }
-
-// function hoverOffSiderbar(){
-//     document.getElementById("sideBar").style.width ="4%";
-// }
 
 // API for cover art
 var repoList = document.querySelector('#coverArt');
