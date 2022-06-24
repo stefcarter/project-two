bdbf1abf690012f7498f84d3655fe682455d2a63
Adam McLane <mclanea92@gmail.com>
2022-06-24 09:49:43 -0400

    updated files

--- public/js/login.js
+++ public/js/login.js
@@ -3,20 +3,24 @@ const signupHandler = async (event) => {
     console.log('inside signup handler')
     
   
-    const email = document.querySelector('#signupemail');
-    const password = document.querySelector('#signuppassword');
+    var email = document.querySelector('#signupemail');
+    var password = document.querySelector('#signuppassword');
+    email = document.getElementById('signupemail').value;
+    password = document.getElementById('signuppassword').value;
 
+    console.log(email, password);
     if (email && password) {
+       
         const response = await fetch('/api/user/signup', {
             method: 'POST',
             body: JSON.stringify({email, password}),
             headers: {'Content-Type': 'application/json'}
         });
         if (response.ok) {
-            document.location.replace('/user')
+            document.location.replace('/user');
         }
         else {
-            alert(response.statusText)
+            alert(response.statusText);
         }
     };
 }
@@ -25,11 +29,12 @@ const signupHandler = async (event) => {
 
 const loginHandler = async (event) => {
     event.preventDefault();
-    console.log('inside login handler')
+    console.log('inside login handler');
     const email = document.querySelector('#loginemail');
     const password = document.querySelector('#loginpassword');
 
     if (password && email) {
+        console.log(email, password);
         const response = await fetch('/api/user/login', {
             method: 'POST',
             body: JSON.stringify({ password, email }),
@@ -38,10 +43,10 @@ const loginHandler = async (event) => {
         console.log(response)
 
         if (response.ok) {
-            document.location.replace('/user')
+            document.location.replace('/user');
         }
         else {
-            alert(response.statusText)
+            alert(response.statusText);
         }
     }
 };
